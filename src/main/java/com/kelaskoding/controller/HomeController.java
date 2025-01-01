f4a4d9ab9972e68619b5382f99be9e4056126119
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2025-01-01 21:06:24 +0700

    menghandle form submission

--- src/main/java/com/kelaskoding/controller/HomeController.java
+++ src/main/java/com/kelaskoding/controller/HomeController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kelaskoding.entity.Product;
 import com.kelaskoding.service.ProductService;
 
 
@@ -23,6 +25,23 @@ public class HomeController {
         model.addAttribute("msg", message);
         model.addAttribute("products", productService.findAll());
         return "index";
+    }
+
+
+    @GetMapping("/add")
+    public String add(Model model){
+        // doing something 
+        String message = "add product";
+        model.addAttribute("msg", message);
+        model.addAttribute("product", new Product());
+        return "add";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product){
+        // doing something 
+        productService.add(product);
+        return "redirect:/";
     }
     
 }
--- src/main/java/com/kelaskoding/service/ProductService.java
+++ src/main/java/com/kelaskoding/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.kelaskoding.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,14 +11,22 @@ import com.kelaskoding.entity.Product;
 @Service
 public class ProductService {
     
-    private static List<Product> products = Arrays.asList(
-        new Product(1L, "001", "Product 1", 50000.0),
-        new Product(2L, "002", "Product 2", 50000.0),
-        new Product(3L, "003", "Product 3", 50000.0),
-        new Product(4L, "004", "Product 4", 50000.0)
+    private static List<Product> products = new ArrayList<>(
+        Arrays.asList(
+            new Product(System.currentTimeMillis(), "001", "Product 1", 50000.0),
+            new Product(System.currentTimeMillis(), "002", "Product 2", 50000.0),
+            new Product(System.currentTimeMillis(), "003", "Product 3", 50000.0),
+            new Product(System.currentTimeMillis(), "004", "Product 4", 50000.0)
+        )
     );
+    
 
     public List<Product> findAll(){
         return products;
+    }
+
+    public void add(Product product){
+        product.setId(System.currentTimeMillis());
+        products.add(product);
     }
 }
